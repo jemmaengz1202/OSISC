2bb7eb87ec5d013db7d3d60fd87fcecb84046de3
Juan Enríquez <jemmaengz1202@gmail.com>
2017-10-01 20:02:46 -0500

    first commit

---
+++ app/src/main/java/com/jemma/isic3/osisc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jemma.isic3.osisc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
